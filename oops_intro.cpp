77e78b51edb648089cf5101fa99a039f0f3adeeb
spacemonkey1101 <ritambh54@gmail.com>
2022-11-13 11:37:57 +0530

    using getters and setters

--- oops_intro.cpp
+++ oops_intro.cpp
@@ -3,21 +3,49 @@ using namespace std;
 
 class Product
 {
+private:
     int id;
     char name[100];
-
-public:
     int mrp;
     int selling_price;
+
+public:
+    // setters
+    void setSellingPrice(int price)
+    {
+        selling_price = price;
+    }
+    void setMRP(int price)
+    {
+        mrp = price;
+    }
+    // getters
+    int getMRP()
+    {
+        return mrp;
+    }
+    int getSellingPrice()
+    {
+        return selling_price;
+    }
 };
 
 int main()
 {
 
     Product camera;
-    camera.mrp = 100;
-    camera.selling_price = 80;
+    // this is not the correct way of setting variables
+    // camera.mrp = 100;
+    // camera.selling_price = 80;
+    // we need to implement getters and setters
+
+    // setting mrp and selling price using setter methods
+    camera.setMRP(1000);
+    camera.setSellingPrice(1200);
     cout << sizeof(camera) << endl; // 4+4+4+100
-    cout << "MRP of camera is" << camera.mrp << endl;
-    cout << "Selling Price of camera is" << camera.selling_price << endl;
+
+    // using getters to get the value
+
+    cout << "MRP of camera is" << camera.getMRP() << endl;
+    cout << "Selling Price of camera is" << camera.getSellingPrice() << endl;
 }
