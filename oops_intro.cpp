70a393082cd82323733feecd8c0f029d85139004
spacemonkey1101 <ritambh54@gmail.com>
2022-11-20 18:26:17 +0530

    empty copy constructor

--- oops_intro.cpp
+++ oops_intro.cpp
@@ -19,17 +19,21 @@ public:
         cout << "Inside constructor" << endl;
     }
 
-    Product(int id, char *n, int mrp, int selling_price){
-        //this is a parameterized constructor that is being used to 
-        //initialize member variables
-        this->id = id ; //left id refers to the 'id' member 
+    Product(int id, char *n, int mrp, int selling_price)
+    {
+        // this is a parameterized constructor that is being used to
+        // initialize member variables
+        this->id = id; // left id refers to the 'id' member
         strcpy(name, n);
         this->mrp = mrp;
         (*this).selling_price = selling_price;
-
     }
-        // setters
-        void setSellingPrice(int price)
+
+    Product (Product &P) {
+        //using an empty copy constructor leads to the copy not taking place
+    }
+    // setters
+    void setSellingPrice(int price)
     {
         // additional checks on setting selling price
         // we should put a price that is lower than or equall to MRP
@@ -62,10 +66,12 @@ public:
     {
         return selling_price;
     }
-    char* getName(){
+    char *getName()
+    {
         return name;
     }
-    int getId(){
+    int getId()
+    {
         return id;
     }
 };
@@ -76,9 +82,8 @@ int main()
     Product camera;
     Product camera2(2, "SonyCam", 2200, 2000);
 
-    //Calling the default copy constructor which is internally implemented
+    // Calling the default copy constructor which is internally implemented
     Product camera3(camera2);
-
 
     // this is not the correct way of setting variables
     // camera.mrp = 100;
@@ -94,13 +99,13 @@ int main()
 
     cout << "MRP of camera is " << camera.getMRP() << endl;
     cout << "Selling Price of camera is " << camera.getSellingPrice() << endl;
-    
+
     cout << "id od camera2 is " << camera2.getId() << endl;
     cout << "name of camera2 is " << camera2.getName() << endl;
     cout << "MRP of camera2 is " << camera2.getMRP() << endl;
     cout << "Selling Price of camera2 is " << camera2.getSellingPrice() << endl;
-    
-    // This will give 
+
+    // This will give
     cout << "id od camera3 is " << camera3.getId() << endl;
     cout << "name of camera3 is " << camera3.getName() << endl;
     cout << "MRP of camera3 is " << camera3.getMRP() << endl;
